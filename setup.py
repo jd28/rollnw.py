30be8b688e02e469a9e1345614500891c6d7b9d0
jmd <joshua.m.dean@gmail.com>
2023-01-31 10:37:19 -0800

    0.10.dev0
    new: basic binary model loading

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="rollnw",
-    version="0.9.dev0",
+    version="0.10.dev0",
     author="jmd",
     author_email="joshua.m.dean@gmail.com",
     include_package_data=True,
